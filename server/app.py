1c8229a63348e9997101e1f6315640ecc7f9899e
bhavyarajas <148911981+bhavyarajas@users.noreply.github.com>
2025-10-30 01:58:04 -0400

    updated server/app.py connection to frontend

--- server/app.py
+++ server/app.py
@@ -16,6 +16,7 @@ origins = [
     "http://localhost:5173",
     "http://127.0.0.1:3000",
     "http://127.0.0.1:5173",
+    "https://scoreboard-app-snowy.vercel.app",
 ]
 app.add_middleware(
     CORSMiddleware,
